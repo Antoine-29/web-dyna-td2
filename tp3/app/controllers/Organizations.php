9adfef04301323f0c30a1a2485cd5e989ce76c6f
antoine pons <21905618@etu.unicaen.fr>
2020-05-07 14:50:26 +0200

    correction erreurs

--- tp3/app/controllers/Organizations.php
+++ tp3/app/controllers/Organizations.php
@@ -1,6 +1,29 @@
+<?php
+namespace controllers;
+use Ubiquity\orm\DAO;
+use models\Organization;
+
+/**
+ * Controller Organizations
+ * @route("organizations")
+ **/
 class Organizations extends \Ubiquity\controllers\ControllerBase{
-    ...
- 
+    
+    /**
+     * @get("name"=>"orgas-index")
+     **/
+    public function index(){
+        $organizations=DAO::getAll(Organization::class);
+        $this->loadView("Organizations/index.html",["orgas"=>$organizations]);
+    }
+/**
+    * @get("{idOrga}/{idGroupe}","name"=>"orgas-display")
+    **/    
+    public function display($idOrga,$idGroupe=null){
+        $orga=DAO::getById(Organization::class, $idOrga,['users','groupes']);
+        $users=$this->users($idOrga,$idGroupe,$orga->getUsers());
+        $this->jquery->renderView("Organizations/display.html",["orga"=>$orga,"users"=>$users]);
+    }
     protected function users($idOrga,$idGroupe=null,$users=null){
         if(isset($idGroupe)){
             $group=DAO::getById(Groupe::class,$idGroupe,['users']);
@@ -10,14 +33,5 @@ class Organizations extends \Ubiquity\controllers\ControllerBase{
             $title="Tous les utilisateurs";
         }
         return $this->loadView("Organizations/users.html",compact("users","title"),true);
-         public function display($idOrga,$idGroupe=null){
-       			 $orga=DAO::getById(Organization::class, $idOrga,['users','groupes']);
-      		     $users=$this->users($idOrga,$idGroupe,$orga->getUsers());
-       			 $this->jquery->renderView("Organizations/display.html",["orga"=>$orga,"users"=>$users]);
     }
-            <ul>
-        {% for groupe in orga.groupes %}
-            <li><a href="{{ path('orga-display',[orga.id,groupe.id]) }}" data-target="#users">{{ groupe }}</a></li>
-        {% endfor %}
-        </ul>
-    }
+}

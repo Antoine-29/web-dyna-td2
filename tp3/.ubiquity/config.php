bccb8751d1a1ed2ebe35c75baff6589c4f752e4b
antoine pons <21905618@etu.unicaen.fr>
2020-05-07 13:28:42 +0200

    tp3 début

---
+++ tp3/.ubiquity/config.php
@@ -0,0 +1,2 @@
+<?php
+return array("host"=>"127.0.0.1","port"=>8090,"sessionName"=>"s5eb3ee0f0d845");
---
+++ tp3/app/cache/git/settings.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("name"=>"web-dyna-td2","remoteUrl"=>"https://github.com/Antoine-29/web-dyna-td2","baseFolder"=>"C:\\xampp\\htdocs\\web-dyna-td2","user"=>"Antoine-29","password"=>"E.arat92");

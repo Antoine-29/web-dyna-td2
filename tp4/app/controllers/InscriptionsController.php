63b31dc6f2d9d04e40559298632a7b842f288dee
antoine pons <21905618@etu.unicaen.fr>
2020-05-13 09:32:18 +0200

    formulaire d'inscription erreur..

--- tp4/app/controllers/InscriptionsController.php
+++ tp4/app/controllers/InscriptionsController.php
@@ -8,4 +8,14 @@ class InscriptionsController extends ControllerBase{
 	public function index(){
 		$this->loadView("InscriptionsController/index.html");
 	}
+
+	/**
+	 *@route("/inscription","methods"=>["post"])
+	**/
+	public function inscription($name="Nom ou pseudo",$mail="Adresse mail"){
+		
+		$this->loadView('InscriptionsController/inscription.php',compact('name','mail'));
+
+	}
+
 }
---
+++ tp4/app/views/InscriptionsController/inscription.php
@@ -0,0 +1,6 @@
+<?php
+<form method ="POST" action = "/inscription/do" >
+<input type="texte" name="Nom ou pseudo">
+<input type="email" mail="Adresse mail">
+<hr>
+<button>Valider l inscription <a href="{{path('Home')}}"</a> </button>
